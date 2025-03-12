676432b20d13e320a071d81fa8dbeee23e96bdd1
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2025-03-12 11:37:07 +0500

    Update InteractionsSection.tsx

--- src/app/components/InteractionsSection.tsx
+++ src/app/components/InteractionsSection.tsx
@@ -168,7 +168,7 @@ const interactions = [
   {
     id: 1,
     leader: "Zainul Abidin Rasheed",
-    description: "A Warm Meeting with Zainul Abidin Rasheed, Singapore’s Ambassador to Kuwait",
+    description: "Zainul Abidin Rasheed, Singapore’s Ambassador to Kuwait and Special Envoy of the Minister for Foreign Affairs",
     image: meet4.src,
     country: "Singapore"
   },

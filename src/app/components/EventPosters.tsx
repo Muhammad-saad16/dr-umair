931d3e6143d639465b0a9add28d21d6a0eb0b71d
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2025-04-11 23:02:04 +0500

    Update EventPosters.tsx

--- src/app/components/EventPosters.tsx
+++ src/app/components/EventPosters.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react"
 import Image from "next/image"
 import Link from "next/link"
-import event1 from "../../../Public/post1.jpg"
+import event1 from "../../../Public/post6.jpg"
 import event2 from "../../../Public/post5.jpg"
 import event3 from "../../../Public/post3.jpg"
 import event4 from "../../../Public/post4.jpg"

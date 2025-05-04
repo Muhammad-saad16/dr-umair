a24ad67b2803bc5ad0be86c743553c24fb70e0e0
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2025-05-04 12:03:41 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,68 +1,19 @@
-// import { Inter } from "next/font/google";
-// import "./globals.css";
-// import Layout from "./components/Layout";
-// import type React from "react";
-// import Head from "next/head";
-// import Image from 'next/image';
-// import type { Metadata } from "next"
-
-// // Initialize English font
-// const inter = Inter({
-//   subsets: ["latin"],
-//   variable: "--font-inter",
-// });
-// export const metadata: Metadata = {
-//   title: "Dr Uamir Mehmood Siddiqui",
-//   description: "",
-//   icons: {
-//     icon: [
-//       {
-//         url: "/favicon.ico",
-//         sizes: "any",
-//       },
-//       {
-//         url: "logo.png",
-//         type: "image/png",
-//         sizes: "180x180",
-//       },
-//     ],
-//   },
-// }
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <html lang="en">
-//       <Head>
-//         {/* Favicon Logo */}
-//       <Image src="logo.png" alt="Drumairsiddl Logo" width={180} height={40} priority />
-//         <title>My Website</title>
-//       </Head>
-//       <body className={inter.variable}>
-//         <Layout>{children}</Layout>
-//       </body>
-//     </html>
-//   );
-// }
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "./components/Layout";
 import type React from "react";
-import type { Metadata } from "next";
+import Head from "next/head";
+import Image from 'next/image';
+import type { Metadata } from "next"
 
 // Initialize English font
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
-
 export const metadata: Metadata = {
   title: "Dr Uamir Mehmood Siddiqui",
-  description: "Official Website",
+  description: "",
   icons: {
     icon: [
       {
@@ -70,13 +21,13 @@ export const metadata: Metadata = {
         sizes: "any",
       },
       {
-        url: "/logo.png",
+        url: "logo.png",
         type: "image/png",
         sizes: "180x180",
       },
     ],
   },
-};
+}
 
 export default function RootLayout({
   children,
@@ -85,6 +36,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Favicon Logo */}
+      <Image src="logo.png" alt="Drumairsiddl Logo" width={180} height={40} priority />
+        <title>My Website</title>
+      </Head>
       <body className={inter.variable}>
         <Layout>{children}</Layout>
       </body>

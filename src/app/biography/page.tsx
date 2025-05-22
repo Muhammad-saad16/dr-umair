c05860ef8ed5f7bcb3ef46e3d6b1c6ba212a053c
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2025-05-22 15:30:58 +0500

    Update page.tsx

--- src/app/biography/page.tsx
+++ src/app/biography/page.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
     // <Layout>
       <div className="container mx-auto px-4 py-12 ">
         <div className="max-w-4xl mx-auto">
-          <h1 className="text-4xl font-bold mb-8 text-center text-white">Meet Dr.Umair</h1>
+          <h1 className="text-4xl font-bold mb-8 text-center text-white"> Dr.Umair</h1>
           
           <div className="mb-12 relative aspect-video overflow-hidden rounded-lg">
             <Image

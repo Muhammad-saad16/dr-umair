5915911b310007e28dcc2d1014da13da3bbfde02
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2025-05-31 18:31:33 +0500

    Update page.tsx

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -167,7 +167,7 @@ export default function GalleryPage() {
         </div> */}
 
         {/* Gallery Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-4">
           {images.map((image) => (
             <div
               key={image.id}

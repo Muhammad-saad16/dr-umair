e00a421d00099c2d2c75de64fd9458243641f389
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2025-06-10 00:11:59 +0500

    Update EventPosters.tsx

--- src/app/components/EventPosters.tsx
+++ src/app/components/EventPosters.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 import Link from 'next/link';
 import event1 from "../../../Public/post14.jpg"
 import event2 from "../../../Public/post13.jpg"
-import event3 from "../../../Public/post12.jpg"
+import event3 from "../../../Public/post16.jpg"
 import event4 from "../../../Public/post15.jpg"
 
 const eventPosters = [

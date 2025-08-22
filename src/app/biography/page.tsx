bb0995ceb6327356fdc5e5f42dce74d171cbfc20
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2025-08-22 23:19:21 +0500

    Update page.tsx

--- src/app/biography/page.tsx
+++ src/app/biography/page.tsx
@@ -1,6 +1,6 @@
 // import Layout from '../components/Layout'
 import Image from 'next/image'
-import dr from '../../../Public/dr-1.jpg'
+import dr from '../../../Public/dr.jpg'
 export default function AboutPage() {
   return (
     // <Layout>
@@ -78,6 +78,7 @@ export default function AboutPage() {
     // </Layout>
   )
 }
+
 
 
 

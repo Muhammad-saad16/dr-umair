94badadb9d6fcb8e890a8072cf3579ed7bdb6b50
Saad <108891916+Muhammad-saad16@users.noreply.github.com>
2025-09-20 23:33:20 +0500

    Update PosterSection.tsx

--- src/app/components/PosterSection.tsx
+++ src/app/components/PosterSection.tsx
@@ -4,10 +4,13 @@ import canada from "../../../Public/canada.jpg"
 export default function PosterSection() {
   return (
     <section className="py-16 bg-[url('/back2.jpg')] object-cover">
+      <h2 className="text-4xl font-bold text-center text-white mb-12">Publications</h2>
       <div className="container mx-auto px-4">
+        
         <Image src={canada} alt="canada Tour" />
       </div>
     </section>
   )
 }
+
 

5aaca15e769ebbb900fa4cecd8f2dee722d47d9a
Muhammad-saad16 <saadforyou20@gmail.com>
2026-08-12 17:36:45 +0500

    updated

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,7 +3,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
-import banner from '../../../Public/new-banner1.jpg'
+import banner from '../../../Public/bann2.jpeg'
 import { routes } from "../lib/routes"
 
 export default function Header() {
